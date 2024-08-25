6a2d9c0d807bd25f30a0471d66bc3d6f0de38853
JamesBerlioz <mavlut18@gmail.com>
2024-08-25 04:54:44 +0300

    feat: create game manager

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,9 @@
+import { _decorator, Component } from "cc";
+const { ccclass } = _decorator;
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+  start() {}
+
+  update(deltaTime: number) {}
+}
